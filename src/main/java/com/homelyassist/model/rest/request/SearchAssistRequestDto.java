1896c8966f25b33290874881f23325bdd71436ea
Homely Assist <homeconnecteasy@gmail.com>
2024-03-11 16:48:16 +0530

    add pending files

---
+++ src/main/java/com/homelyassist/model/rest/request/SearchAssistRequestDto.java
@@ -0,0 +1,26 @@
+package com.homelyassist.model.rest.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.homelyassist.model.enums.AgriculturalAssistType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchAssistRequestDto {
+
+    @JsonProperty("assist_types")
+    private List<AgriculturalAssistType> assistTypes;
+
+    @JsonProperty("city_area")
+    private String cityArea;
+
+    @JsonProperty("pin_code")
+    private String pinCode;
+}
