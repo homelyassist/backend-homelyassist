d6bc584c7b4f9c9f4dda33ad114bf681d014eb9c
amaresh.nayak6@gmail.com <amaresh.nayak6@gmail.com>
2024-03-20 20:26:17 +0530

    InitialUI CheckIN

--- src/main/java/com/homelyassist/controller/AdminController.java
+++ src/main/java/com/homelyassist/controller/AdminController.java
@@ -2,9 +2,14 @@ package com.homelyassist.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
 
     /**
      *
